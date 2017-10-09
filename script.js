04ea96aa3bbfb9f61739a335fd838ec25f5c784b
KyKy2017 <32532074+KyKy2017@users.noreply.github.com>
2017-10-09 16:07:30 +1300

    intial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var URL = "http://challengekypham.azurewebsites.net/api/getUsers";
+
+function showAlert() {
+    var name = document.getElementById("name").value;
+    for (var i = 0; i < name.length; i++) {
+        console.log(name[i]);
+
+    }
+    var alpha = {a:1, b:2, c:3};
+    console.log(alpha[name[0]]);
+    document.getElementById("result").innerHTML = "Result " + name.length;
+}
+
+//Posted from jdgg1993//
+
+function postRecord() {
+    var name = document.getElementById("name").value;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("result").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", URL + "?username=" + name, true);
+    xhttp.send();
+}
+
+function getRecord() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            for (var i = 0; i < response.length; i++) {
+              document.getElementById("result").innerHTML += response[i].Name + "<br> ";
+            }
+        }
+    };
+    xhttp.open("GET", URL, true);
+    xhttp.send();
+}
